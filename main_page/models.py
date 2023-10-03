dc1bd16de2fe63e6d0aa1aa5731094713dee0142
Emily Jarecki <E.Jarecki@shopmedvet.com>
2023-10-03 14:43:58 -0500

    added bootstrap. adjusted models. made responsive.

--- main_page/models.py
+++ main_page/models.py
@@ -9,13 +9,13 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=80)
-    SKU = models.CharField(max_length=80)
-    description = models.CharField(max_length=300)
+    name = models.CharField()
+    SKU = models.CharField()
+    description = models.CharField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     vendor = models.CharField(max_length=80)
-    price = models.IntegerField()
-    specialPrice = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    specialPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return self.name()
