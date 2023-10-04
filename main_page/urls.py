7b79c804322334760241e55bf2c400dfc2786334
Emily Jarecki <e.jarecki@shopmedvet.com>
2023-10-04 16:30:17 -0500

    working on POST

--- main_page/urls.py
+++ main_page/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:category_id>/", views.detail, name="detail"),
     path("product/<int:product_id>/", views.product_detail, name="product_detail"),
+    path('import/', views.import_from_excel, name='import_from_excel'),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from openpyxl import load_workbook
 import pandas as pd
 
 from .models import Category, Product
@@ -11,22 +13,43 @@ def index(request):
 
     dataframe1 = pd.read_excel(r"C:\Users\E.Jarecki\OneDrive - American Health Services\Desktop\Book1.xlsx")
     df = pd.DataFrame(dataframe1)
-    name_list = df['Name'].tolist()
+    name_list = df['name'].tolist()
+
+    # the entire excel info
+    print(df)
+
+    # just the names
     print(name_list)
 
     context = {'category_list': category_list, 'product_name': name_list}
-    
+
     return render(request, 'main_page/index.html', context)
 
 def detail(request, category_id):
-    # category = Category.objects.get(pk=category_id)
     category = get_object_or_404(Category, pk=category_id)
     products = category.product_set.all()
     context = {'category': category, 'products': products}
     return render(request, "main_page/detail.html", context)
 
 def product_detail(request, product_id):
-    # product = Product.objects.get(pk=product_id)
     product = get_object_or_404(Product, pk=product_id)
     context = {"product": product}
     return render(request, "main_page/product_detail.html", context)
+
+def import_from_excel(request):
+    print(request.method.excel_file)
+    if request.method == "GET":
+        return render(request, 'main_page/import_form.html')
+    if request.method == "POST":
+        print(request)
+        # excel_file = request.FILES[r"C:\Users\E.Jarecki\OneDrive - American Health Services\Desktop\Book1.xlsx"]
+        # wb = load_workbook(excel_file)
+        # ws = wb.active
+        return render(request, 'main_page/import_form.html')
+
+    #     for row in ws.iter_rows(min_row=7, values_only=True):
+    #         name, SKU, description, vendor, price, specialPrice, category_id = row
+    #         Product.objects.create(name=name, SKU=SKU, description=description, vendor=vendor, price=price, specialPrice=specialPrice, category_id=category_id)
+        
+    #     return render(request, 'main_page/import_success.html')
+    # else: 
