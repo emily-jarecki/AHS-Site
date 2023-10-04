00d3261a155c1f105f3366625ade64ce99e360c9
Emily Jarecki <E.Jarecki@shopmedvet.com>
2023-10-04 12:10:54 -0500

    able to display item names from static excel file

--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
+import pandas as pd
 
 from .models import Category, Product
 
@@ -7,7 +8,14 @@ from .models import Category, Product
 def index(request):
     category_list = Category.objects.all()
     print(category_list)
-    context = {'category_list': category_list}
+
+    dataframe1 = pd.read_excel(r"C:\Users\E.Jarecki\OneDrive - American Health Services\Desktop\Book1.xlsx")
+    df = pd.DataFrame(dataframe1)
+    name_list = df['Name'].tolist()
+    print(name_list)
+
+    context = {'category_list': category_list, 'product_name': name_list}
+    
     return render(request, 'main_page/index.html', context)
 
 def detail(request, category_id):
