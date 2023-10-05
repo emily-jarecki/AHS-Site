6b9754b928958dc959f4f6b3600e4f51b23c9256
Emily Jarecki <e.jarecki@shopmedvet.com>
2023-10-05 08:38:03 -0500

    excel info that is uploaded is printing into terminal

--- main_page/views.py
+++ main_page/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
+import openpyxl
 from openpyxl import load_workbook
 import pandas as pd
 
@@ -11,14 +12,15 @@ def index(request):
     category_list = Category.objects.all()
     print(category_list)
 
+    # HARDCODE LOCATION INTO INDEX PAGE - NOT IDEAL 
     dataframe1 = pd.read_excel(r"C:\Users\E.Jarecki\OneDrive - American Health Services\Desktop\Book1.xlsx")
     df = pd.DataFrame(dataframe1)
     name_list = df['name'].tolist()
 
-    # the entire excel info
+    # ENTIRE EXCEL INFO
     print(df)
 
-    # just the names
+    # JUST THE NAMES COLUMN
     print(name_list)
 
     context = {'category_list': category_list, 'product_name': name_list}
@@ -37,19 +39,43 @@ def product_detail(request, product_id):
     return render(request, "main_page/product_detail.html", context)
 
 def import_from_excel(request):
-    print(request.method.excel_file)
     if request.method == "GET":
+        print("hello world")
         return render(request, 'main_page/import_form.html')
+    
     if request.method == "POST":
-        print(request)
-        # excel_file = request.FILES[r"C:\Users\E.Jarecki\OneDrive - American Health Services\Desktop\Book1.xlsx"]
-        # wb = load_workbook(excel_file)
-        # ws = wb.active
-        return render(request, 'main_page/import_form.html')
 
-    #     for row in ws.iter_rows(min_row=7, values_only=True):
-    #         name, SKU, description, vendor, price, specialPrice, category_id = row
-    #         Product.objects.create(name=name, SKU=SKU, description=description, vendor=vendor, price=price, specialPrice=specialPrice, category_id=category_id)
+        # TYPE OF REQUEST
+        print(request)
+        excel_file = request.FILES["excel_file"]
+
+        # THE TITLE OF EXCEL FILE
+        print(excel_file)
+        wb = load_workbook(excel_file)
+
+        #  CRAZY: <openpyxl.workbook.workbook.Workbook object at 0x000001A8590C2D50>
+        print("WB: ", wb)
         
-    #     return render(request, 'main_page/import_success.html')
-    # else: 
+        # GETTING SHEET1 -  as a precaution
+        worksheet = wb["Sheet1"]
+
+        # <Worksheet "Sheet1">
+        print("Worksheet: ", worksheet)
+
+        # WHATEVER VALUE IS IN A1 
+        # ONLY A SINGLE VALUE
+        print(worksheet["A1"].value)
+
+        excel_data = list()
+
+        # iterating over the rows and getting value from each cell in row
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+            excel_data.append(row_data)
+
+            # ALL ROWS 
+            print(row_data)
+        
+        return render(request, 'main_page/import_form.html', {"excel_data":excel_data})
